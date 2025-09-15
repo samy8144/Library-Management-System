c959c502efb0ee74d5b0c1d26d0861ca9ba196ff
samy8144 <samy8144@users.noreply.github.com>
2025-09-15 11:11:50 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,23 +1,19 @@
+var mysql = require("mysql2");
 
-
-var mysql = require("mysql");
-
-var mysql=require("mysql2")
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "lib"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 con.connect(function(err){
-    if (err)   throw err;
-      else console.log("connected");
-      con.query("select * from books",function(err,result){
-          if (err) throw err;
-          else console.log(result);
-      })
-  });
-  
+    if (err) throw err;
+    else console.log("connected");
+    con.query("select * from books", function(err, result){
+        if (err) throw err;
+        else console.log(result);
+    });
+});
 
-module.exports=con;
+module.exports = con;
